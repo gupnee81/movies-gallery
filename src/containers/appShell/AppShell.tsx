4e544482bafd320ef84968ae5521df47d518b7eb
gupnee81 <neelima.gupta@iselect.com.au>
2020-07-27 00:42:44 +1000

    added UI design screenshot

--- src/containers/appShell/AppShell.tsx
+++ src/containers/appShell/AppShell.tsx
@@ -3,14 +3,12 @@
  * This component has all routers defined for an application.
  *
  */
-// tslint:disable: prettier
 import React from 'react';
 import SearchPage from 'Containers/home';
 import { Switch, Route } from 'react-router-dom';
 import PageNotFound from 'Containers/pageNotFound';
 
 class AppShell extends React.Component<{}, {}> {
-
   public render() {
     return (
       <React.Fragment>
